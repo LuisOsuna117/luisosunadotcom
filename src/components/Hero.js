990de86eb1a3eccfed6067d10c25e07390a7dc3d
Luis Osuna <hack.jlom@gmail.com>
2024-10-22 18:47:44 -0700

    feat(s3-deploy): Adding build and sync to S3 process to deploy workflow

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,23 @@
 import profile from '../assets/profile.jpeg';
 import Button from './Button';
-import { FaLinkedin, FaFilePdf } from "react-icons/fa";
+import { FaLinkedin, FaFilePdf, FaAngleDown } from "react-icons/fa";
+import { useState, useEffect } from 'react';
 
 function Hero() {
+    const [isBouncing, setIsBouncing] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY > 0) {
+            setIsBouncing(false);  // Stop bouncing when scrolling
+          }
+        };
+        window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
     return (
         <div className="h-lvh bg-amazon flex flex-col justify-center items-center text-white">
             <img src={profile} className="w-40 rounded-full" alt="" />
@@ -10,9 +25,11 @@ function Hero() {
             <h3 className='font-sans text-xl mt-1 text-center'>DevOps Engineer | Cloud Solutions Architect</h3>
             <div className='flex flex-col sm:flex-row'>
                 <Button icon={FaLinkedin} color="azure" title="Get in touch" to="https://www.linkedin.com/in/luisosuna117" />
-                <Button icon={FaFilePdf} color="amber" title="Download CV" to="/downloads/resume.pdf" />
+                <Button icon={FaFilePdf} color="amber" title="Get my resume" to="/downloads/resume.pdf" />
             </div>
-
+            <div className={`${isBouncing ? 'animate-bounce' : ''} font-sans flex flex-row font-light absolute bottom-0`}>
+                <FaAngleDown className='mt-1 mx-2'/>Scroll to find out more<FaAngleDown className='mt-1 mx-2'/>
+            </div>
         </div>
     );
 }
