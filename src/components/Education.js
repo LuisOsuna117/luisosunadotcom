903f25f11e39d4edd642a6befef5f86c65e40480
Luis Osuna <hack.jlom@gmail.com>
2024-11-02 20:58:11 -0700

    feat(Home): Adding about me, work history, certs and  education

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,7 +5,7 @@ function Education() {
     return (
         <div className="divide-y divide-black/87 flex flex-col items-center p-4">
             {EducationData.map((item) => (
-                <Item img={item.image} title={item.title} dates={item.dates} desc={item.description} />
+                <Item key={item.id} img={item.image} title={item.title} dates={item.dates} desc={item.description} />
             ))}
         </div>
     );
