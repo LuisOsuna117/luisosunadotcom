1b9fe899b85eea0cfcf320dda86ee1088a305120
Luis Osuna <hack.jlom@gmail.com>
2024-11-02 21:26:16 -0700

    fix(cloudfront): always redirect http to https

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ function Item(props) {
     return (
         <div className="flex flex-col sm:flex-row items-center w-11/12 2xl:w-2/4 py-2">
             <img className="h-20 w-20 rounded" src={img} alt="" />
-            <div className="flex flex-col px-4">
+            <div className="w-full flex flex-col px-4">
                 <div className="font-bold">{title}</div>
                 <div className="font-light text-gray-600">{dates}</div>
                 <div className="text-sm text-justify">{desc}</div>
