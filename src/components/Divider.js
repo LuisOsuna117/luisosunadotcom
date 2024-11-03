fd2f493c46614d2e4a32b1a975e761c27175f0be
Luis Osuna <hack.jlom@gmail.com>
2024-11-02 21:10:37 -0700

    fix(React): importing PropTypes

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 function Divider(props) {
     const { title } = props
     return (
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 function Item(props) {
     const { img, title, dates, desc } = props
     return (
